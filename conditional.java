59dabb5d182b5375c89953a0d4abd6bdfbb015db
shivi150ka <pes2003annu@gmail.com>
2024-12-17 11:57:11 +0530

    new problem

--- conditional.java
+++ conditional.java
@@ -32,7 +32,7 @@ public class conditional {
         sc.close();
     }
 }*/
-import java.util.Scanner;
+/*import java.util.Scanner;
 public class conditional {
  public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -49,4 +49,79 @@ else{
 sc.close();
  }
     
+}*/
+/*import java.util.Scanner;
+public class conditional {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your first number=");
+        int num1 = sc.nextInt();
+        System.out.println("Your first number="+num1);
+        System.out.println("Enter your second number=");
+        int num2 = sc.nextInt();
+        System.out.print("Your first number="+num2);
+        System.out.println("Enter your third number=");
+        int num3 = sc.nextInt();
+        System.out.println("Your third number="+num3);
+        if((num1>num2)&&(num1>num3))
+        {
+            System.out.println("first number is greatest");
+        }
+        else if ((num2>num1)&&(num2>num3)) {
+            System.out.println("second number is greatest");
+        }
+        else {
+            System.out.println("third number is greatest");
+        }
+        sc.close();
+    }
+}*/
+/*public class conditional {
+
+    public static void main(String[] args) {
+        int a=50;
+        System.out.println(a);
+        int b=40;
+        System.out.println(b);
+         int greater=(a>b)?a:b;
+         System.out.println(greater);
+    }
+}*/
+
+//why do not run the code using nextLine
+
+import java.util.Scanner;
+public class conditional {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        System.err.println("Your first operand ="+num1);
+        int num2 = sc.nextInt();
+        System.err.println("Your second operand ="+num2);
+        char operator = sc.next().charAt(0);
+        System.out.println(operator);
+        switch (operator) {
+            case '+':
+                System.out.println(num1+num2);
+                break;
+                case '-':
+                System.out.println(num1-num2);
+                break;
+                case '*':
+                System.out.println(num1*num2);
+                break;
+                case '/':
+                System.out.println(num1/num2);
+                break;
+                case '%':
+                System.out.println(num1%num2);
+                break;
+            default:
+            System.out.println("You have entered an wrong operator");
+                break;
+        }
+sc.close();
+    }
 }
