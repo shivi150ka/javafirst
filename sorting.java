b1cb6535d73f34a0e0360b90e1f3192ff995dea4
shivi150ka <pes2003annu@gmail.com>
2024-12-19 10:38:48 +0530

    bubble

--- sorting.java
+++ sorting.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 import java.util.Arrays;
 public class sorting {
 public static void inbuiltsort(int numbers[]) {
@@ -21,6 +21,37 @@ public static void printarr(int arr[]){
         inbuiltsort(arr);
         printarr(arr);
         sc.close();
+
+    }
+}*/
+//Bubble Sort
+public class sorting {
+    public static void bubblesort(int number[]){
+        for(int i=0; i<number.length-1;i++)
+        {
+            for(int j=0; j<number.length-i-1;j++ )
+            {
+                if(number[j]>number[j+1])
+                {
+                    int temp = number[j+1];
+                    number[j+1]=number[j];
+                    number[j]= temp;
+
+                }
+            }
+        }
+    }
+    public static void printarr(int arr[]){
+        for(int i=0; i<arr.length;i++){
+            System.out.println(arr[i]+"");
+        }
+        System.out.println();}
+
+    public static void main(String[] args) {
+        int arr[]={8,5,7,2,4,1};
+        bubblesort(arr);
+        printarr(arr);
+        
 
     }
 }
