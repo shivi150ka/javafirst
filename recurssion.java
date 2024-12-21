458d546672acb07b38b5df01b1695882735ecb94
shivi150ka <pes2003annu@gmail.com>
2024-12-21 10:47:25 +0530

    fact

--- recurssion.java
+++ recurssion.java
@@ -1,4 +1,4 @@
-public class recurssion {
+/*public class recurssion {
 public static int printsum(int n){
     if(n==1){
         return 1;
@@ -6,13 +6,26 @@ public static int printsum(int n){
     printsum(n-1);
     int sum = n + printsum(n-1);
     return sum;
-
 }
-
-
-    public static void main(String[] args) {
-        int num=2;
-       
+ public static void main(String[] args) {
+        int num=2;      
 System.out.println( printsum(num));
+    }
+}*/
+public class recurssion {
+public static int fact(int num){
+    if(num==0){
+        return 1;
+    }
+    if(num==1){
+        return 1;
+    }
+    int ans = num*fact(num-1);
+    return ans;
+}
+    public static void main(String[] args) {
+        int n =5;
+        System.out.println(fact(n));
+
     }
 }
