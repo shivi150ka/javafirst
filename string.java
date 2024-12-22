b497f3da566b87d3bcd6544348d9e267fe8c3224
shivi150ka <pes2003annu@gmail.com>
2024-12-22 09:12:31 +0530

    string

---
+++ string.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class string {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.next();
+        System.out.println(name);
+        sc.close();
+    }
+}
