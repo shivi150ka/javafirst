3128d6217317b67a572a97a94fbc0623f56fcb59
Annu Kumari <168550256+shivi150ka@users.noreply.github.com>
2024-12-22 09:23:32 +0530

    Update string.java

--- string.java
+++ string.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class string {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name;
+        String nam;
         name = sc.next();
         System.out.println(name);
 String firstname="Shradha" ;
