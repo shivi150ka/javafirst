41e4e9e7b2e91f4df909c17bc5d13617aab80f45
shivi150ka <pes2003annu@gmail.com>
2024-12-24 07:58:20 +0530

    operation

--- Arraylist.java
+++ Arraylist.java
@@ -1,4 +1,17 @@
 import java.util.*;
 public class Arraylist {
+    public static void main(String[] args) {
+    ArrayList <Integer> list = new ArrayList<>();
     
+   /*for(int i=0;i<=list.size()-1;i++){
+sc.(list.add(i));*/
+    
+   list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+   for(int j=0;j<=list.size()-1;j++){
+        System.out.println("Enter your number ="+list.get(j));
+            }
+}
 }
