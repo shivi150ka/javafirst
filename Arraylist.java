9ac76dbfd23a589aec0354aa726fdac26853ed4b
shivi150ka <pes2003annu@gmail.com>
2024-12-25 08:43:00 +0530

    reverse

--- Arraylist.java
+++ Arraylist.java
@@ -1,4 +1,4 @@
-import java.util.*;
+/*import java.util.*;
 public class Arraylist {
     public static void main(String[] args) {
     ArrayList <Integer> list = new ArrayList<>();
@@ -6,7 +6,7 @@ public class Arraylist {
    /*for(int i=0;i<=list.size()-1;i++){
 sc.(list.add(i));*/
     
-   list.add(1);
+   /*list.add(1);
     list.add(2);
     list.add(3);
     list.add(4);
@@ -22,4 +22,23 @@ sc.(list.add(i));*/
                         System.out.println("Enter your number ="+list.get(j));
                             }
 }
+}*/
+import java.util.*;
+public class Arraylist {
+    public static void reverse (ArrayList<Integer> limb ){
+        for(int i=limb.size()-1;i>=0;i--){
+            System.out.print(limb.get(i)+" ");
+        }
+System.out.println();
+    }
+public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(5);
+    list.add(2);
+    list.add(6);
+    list.add(9);
+    list.add(8);
+    reverse(list);
+}
+    
 }
