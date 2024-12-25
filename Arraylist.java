5caf493954da50ccbaa6fabeb798930b9cd42ffc
Annu Kumari <168550256+shivi150ka@users.noreply.github.com>
2024-12-25 08:53:53 +0530

    Update Arraylist.java

--- Arraylist.java
+++ Arraylist.java
@@ -42,11 +42,11 @@ System.out.println();
     }
 public static void main(String[] args) {
     ArrayList<Integer> list = new ArrayList<>();
-    list.add(5);
-    list.add(2);
-    list.add(6);
-    list.add(9);
     list.add(8);
+    list.add(3);
+    list.add(2);
+    list.add(7);
+    list.add(5);
     reverse(list);
     maxnum(list);
 }
