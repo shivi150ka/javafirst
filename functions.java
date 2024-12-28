39a39d22cf19cde1c846af9e3fee0c45a9c0e79a
shivi150ka <pes2003annu@gmail.com>
2024-12-28 10:55:29 +0530

    prime

--- functions.java
+++ functions.java
@@ -59,7 +59,9 @@ public static boolean isPrime(int num){
 if (num==2) {
     return true;   
 }
-for(int i=2;i<num-1;i++){
+//for(int i=2;i<num-1;i++)
+for (int i=2; i<=Math.sqrt(num);i++)
+{
     if(num%i==0){
         return false;
     }
