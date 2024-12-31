86d250e77983907ff71ae38b1f01ed489aeb2645
shivi150ka <pes2003annu@gmail.com>
2024-12-31 19:18:45 +0530

    conquer

--- divideconquer.java
+++ divideconquer.java
@@ -1,5 +1,5 @@
 public class divideconquer {
-public static void quicksort(int arr[],int si int ei){
+public static void quicksort(int arr[],int si,int ei){
     if(si>=ei){
         return;
     }
@@ -8,9 +8,33 @@ public static void quicksort(int arr[],int si int ei){
     quicksort(arr, pind+1,ei);
 }
 
- public static int partition(int)
+ public static int partition(int arr[],int si,int ei){
+    int pivot=arr[ei]; 
+    int i=si-1;
+    for(int j=si;j<ei;j++){
+        if(arr[j]<=pivot){
+            i++;
+            int temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    i++;
+    int temp=pivot;
+    arr[ei]=arr[i];
+    arr[i]=temp;
+    return i;
+
+ }
+ public static void printarr(int arr[]){
+    for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]);
+    }
+ }
 
     public static void main(String[] args) {
-        
+        int arr[]={5,1,8,9,6};
+        quicksort(arr,0,arr.length-1);
+        printarr(arr);
     }
 }
