348749a9e32d6e1cc66d4263680bfce973e33721
shivi150ka <pes2003annu@gmail.com>
2025-01-01 15:12:10 +0530

    binarytree

---
+++ binarytree.java
@@ -0,0 +1,31 @@
+public class binarytree {
+    static class node{
+        int data;
+        node left;
+        node right;
+        node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class binarytree{
+        static int idx=-1;
+        public static node buildtree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            node newnode=new node(nodes[idx]);
+            newnode.left=buildtree(nodes);
+            newnode.right=buildtree(nodes);
+            return newnode;
+        }
+    }
+    public static void main(String[] args) {
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6};
+        binarytree tree=new binarytree();
+        node root=tree.buildtree(nodes);
+        System.out.println(root.data);
+    }
+}
