c2edea6ab313006d24b7f79c150816f325d95091
shivi150ka <pes2003annu@gmail.com>
2025-01-02 15:42:35 +0530

    errorfix

--- string.java
+++ string.java
@@ -26,7 +26,7 @@ public static String toUpperCase(String str){
     char ch=Character.toUpperCase(str.charAt(0));
     sb.append(ch);
     for(int i=1;i<str.length();i++){
-        if (str.charAt(i)==""&&i<str.length()-1) {
+        if (str.charAt(i)==' ' && i<str.length()-1) {
             sb.append(str.charAt(i));
             i++;
             sb.append(Character.toUpperCase(str.charAt(i)));
@@ -38,7 +38,7 @@ public static String toUpperCase(String str){
     return sb.toString();
 }
     public static void main(String[] args) {
-        String str="Hello WORLD";
-        toUpperCase(str);
+        String str="Hello world";
+       System.out.println(toUpperCase(str)); 
     }
 }
