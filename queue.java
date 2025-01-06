9b701700ae3a30363942ff4cb5948f67b1f0dafa
shivi150ka <pes2003annu@gmail.com>
2025-01-06 07:16:05 +0530

    stack using deque

--- queue.java
+++ queue.java
@@ -113,7 +113,7 @@ public static void main(String[] args)  {
     System.out.println();
 }
 }*/
-import java.util.LinkedList;
+/*import java.util.LinkedList;
 import java.util.Queue;
 public class queue {
 public static void printnonrepeating(String st){
@@ -139,4 +139,33 @@ public static void printnonrepeating(String st){
         String str="uyrsyrtp";
         printnonrepeating(str);
     }
+}*/
+//Implemenyt stack using Deque
+
+import java.util.Deque;
+import java.util.LinkedList;
+public class queue {
+ public static void main(String[] args) {
+    Stack s=new Stack();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    System.out.println("peek element is "+s.peek());
+    System.out.println(" 1st pop "+s.pop());
+    System.out.println("2nd "+s.pop());
+    }
+}
+static class Stack {
+Deque<Integer>deque=new LinkedList<>();
+public void push(int data){
+    deque.addLast(data);
+}
+  public int pop(){
+    return deque.removeLast();
+  }  
+  public int peek(){
+    return deque.getLast();
+  }
 }
