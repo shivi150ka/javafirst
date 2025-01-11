5127b967b36d103f567bed624a8ca663fa5a1aa3
shivi150ka <pes2003annu@gmail.com>
2025-01-11 21:43:56 +0530

    array

--- Arraylist.java
+++ Arraylist.java
@@ -61,9 +61,18 @@ public static void main(String[] args) {
 }
     
 }*/
-import java.util.*;
-public class Arraylist {
 
+import java.util.ArrayList;
+public class Arraylist {
+public static void main(String[] args) {
     ArrayList<Integer> list=new ArrayList<>();
-    list.add(1)
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(1,9);
+    System.out.println();
+    list.get(0);
+    System.out.println(contains);
+}
+   
 }
