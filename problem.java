43a3e2b119c42fd3cbb75e0e6b5af117716a2416
shivi150ka <pes2003annu@gmail.com>
2025-01-13 09:48:56 +0530

    to check

--- problem.java
+++ problem.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 public class problem {
  public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -14,6 +14,21 @@ System.out.println("enter the number" + (3.14*radius*radius));
 sc.close();
  }
     
+}*/
+public class problem {
+public static boolean check(int num[]){
+    for(int i=0;i<num.length;i++){
+        for(int j=1;j<num.length;j++){
+            if(num[i]==num[j]){
+                return true;
+            }
+        }
+    }
+}
+    public static void main(String[] args) {
+        int arr[]={5,6,3,2};
+       boolean ans=check(arr);
+    }
 }
     
 
