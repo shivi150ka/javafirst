ca78b8902de00e69108200ef49ceb74016d24e7f
shivi150ka <pes2003annu@gmail.com>
2025-01-14 18:05:05 +0530

    insert

---
+++ hashmap.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class hashmap {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hm=new HashMap<>();
+        hm.put("india", 600);
+        hm.put("china", 900);
+        hm.put("bhutan", 500);
+        System.out.println(hm);
+    }
+}
