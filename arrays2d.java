de7178687a35773d9a9d018978b8f382563a9602
shivi150ka <pes2003annu@gmail.com>
2025-01-15 19:46:19 +0530

    search

--- arrays2d.java
+++ arrays2d.java
@@ -1,9 +1,21 @@
 public class arrays2d {
     public static boolean sortedmatrix(int num[][],int key){
-        int row=0,coloum=num[0].length;
-        while (condition) {
+        int row=0,col=num[0].length-1;
+        while (row<num.length&&col>=0) {
+            if(num[row][col]==key){
+                System.out.println("found at"+row+col);
+                return true;
             
+            }
+            else if(key<num[row][col]){
+                col--;
+            }
+            else{
+                row++;
+            }
         }
+        System.out.println(" not found"+row+col);
+        return false;
     }
     public static void main(String[] args) {
         int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
