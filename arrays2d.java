d92fc31dd67b48f2f87d28400d20ebee0a32dfa1
shivi150ka <pes2003annu@gmail.com>
2025-01-15 19:04:45 +0530

    search

---
+++ arrays2d.java
@@ -0,0 +1,13 @@
+public class arrays2d {
+    public static boolean sortedmatrix(int num[][],int key){
+        int row=0,coloum=num[0].length;
+        while (condition) {
+            
+        }
+    }
+    public static void main(String[] args) {
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int key=8;
+        sortedmatrix(arr,key);
+    }
+}
