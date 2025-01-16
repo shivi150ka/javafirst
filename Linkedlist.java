5b496d3192d58db4ba2b2807851d308c6ddefd75
shivi150ka <pes2003annu@gmail.com>
2025-01-16 22:23:25 +0530

    create ll

--- Linkedlist.java
+++ Linkedlist.java
@@ -27,4 +27,19 @@ public static void main(String[] args) {
     ll.addfirst(2);
 }
 }*/
-
+public class Linkedlist {
+public static class Node {
+int data;
+Node next;
+public Node(int data){
+    this.data=data;
+    this.next=null;
+}
+    
+}
+public static Node head;
+public static Node tail;
+    public static void main(String[] args) {
+        
+    }
+}
