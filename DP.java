54e4a976fe30871c3fef0420c0d49dfc616babd5
shivi150ka <pes2003annu@gmail.com>
2025-01-30 23:43:44 +0530

    change

--- DP.java
+++ DP.java
@@ -1,5 +1,5 @@
 public class DP {
     //here we learn DP is optimised code of recurssion.
     //DP stands for dynamic programming"
-    
+    //you have to learn and give time for what do you want
 }
