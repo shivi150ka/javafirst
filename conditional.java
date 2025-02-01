dda093bf2211bda4a2d3e7fc78aaad4083d47558
shivi150ka <pes2003annu@gmail.com>
2025-02-01 23:42:29 +0530

    weekend

--- conditional.java
+++ conditional.java
@@ -195,11 +195,48 @@ public class conditional {
         }
     }
 }*/
-import java.util.*;
+/*import java.util.*;
 public class conditional {
 
     for(int i=0;i<5;i++){
         System.out.println("person");
+    }
+}*/
+import java.util.*;
+public class conditional {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n);
+        sc.close();
+        switch (n) {
+            case 1:
+            System.out.println("Monday");
+                break;
+                case 2:
+                System.out.println("Tuesday");
+                    break;
+                    case 3:
+            System.out.println("Wednesday");
+                break;
+                case 4:
+            System.out.println("Thursday");
+                break;
+                case 5:
+            System.out.println("Friday");
+                break;
+                case 6:
+            System.out.println("Saturday");
+                break;
+                case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+            System.out.println("Error");
+                break;
+        }
+
     }
 }
 
