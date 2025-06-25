e59372b8fb69c1063fce499f0682088f5beaaf4c
shivi150ka <pes2003annu@gmail.com>
2025-06-25 10:42:27 +0530

    floyd

--- fun.java
+++ fun.java
@@ -1,11 +1,13 @@
 public class function{
     public static void main(String[] args) {
+          int count=1;
         for(int i=1;i<=5;i++){
          
-            for(int j=1;j<=6-i;j++){
-               System.out.print(j);
+            for(int j=1;j<=i;j++){
               
-                
+               System.out.print(count);
+              
+                count++;
             }
             System.out.println();
         }
