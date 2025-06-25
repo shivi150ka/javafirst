75b109e0c28619698dbcd4919c5c997f6bc9b49e
shivi150ka <pes2003annu@gmail.com>
2025-06-25 11:30:32 +0530

    loop

--- fun.java
+++ fun.java
@@ -1,14 +1,27 @@
 public class function{
     public static void main(String[] args) {
-          int count=1;
-        for(int i=1;i<=5;i++){
-         
-            for(int j=1;j<=i;j++){
-              
-               System.out.print(count);
-              
-                count++;
-            }
+        for(int i=1;i<=4;i++){
+          for(int j=1;j<=i;j++){
+               System.out.print("*");
+             }
+             for(int k=1;k<=8-2*i;k++){
+                System.out.print(" ");
+             }
+ for(int l=1;l<=i;l++){
+               System.out.print("*");
+             }
+            System.out.println();
+        }
+          for(int i=4;i>=1;i--){
+           for(int j=1;j<=i;j++){
+               System.out.print("*");
+             }
+             for(int k=1;k<=8-2*i;k++){
+                System.out.print(" ");
+             }
+ for(int l=1;l<=i;l++){
+               System.out.print("*");
+             }
             System.out.println();
         }
     }
